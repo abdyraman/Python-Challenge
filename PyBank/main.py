0e2438f67e65edca248e530df140330631387902
abdyraman <abdyramanova.j@gmail.com>
2023-03-07 17:45:54 -0500

    still doubtfull

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,9 +12,37 @@ proflosses=[]
 with open(read_filepath, "r") as f:
     reader= csv.reader(f, delimiter=",")
 
+ # Read the header row first (skip this part if there is no header)
+    csv_header = next(f)
+    print(f"Header: {csv_header}")
+
     for row in reader:
         #print(row[0], row[1]) just to check if filepath is correct
         date.append(row[0])
-        proflosses.append(row[1])
+        proflosses.append (int(row[1]))
+
+#Total months
 print (len(date))
 
+#Total Profits losses
+print (sum(proflosses))
+
+#Average Change
+# def average(numbers):
+#     length = len(numbers)
+#     total = 0.0
+#     for number in numbers:
+#         total += number
+#     return total / length
+
+# print(average(proflosses))
+
+
+
+#Greatest increase in profits
+max_value = max(proflosses)
+print(max_value)
+
+#Greatest decrease in profits
+min_value = min(proflosses)
+print(min_value)
